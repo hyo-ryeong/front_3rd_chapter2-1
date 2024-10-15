119457136a832132585c96abe475a7bcd0270dbe
hyo-ryeong <jhl9071@naver.com>
2024-10-16 01:06:01 +0900

    [FEAT] : 기본 세팅

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended, eslintConfigPrettier]
